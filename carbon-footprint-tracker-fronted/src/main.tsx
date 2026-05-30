4107abf9c09bf3d81e31664000920262ac950d43
Rupeshkumar663 <rupeshsamrat08@gmail.com>
2026-05-30 21:43:09 +0530

    Fix Railway build

--- carbon-footprint-tracker-fronted/src/main.tsx
+++ carbon-footprint-tracker-fronted/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -7,11 +7,11 @@ import { store } from "./redux/store";
 import "./index.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
+  
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </StrictMode>
+  
 );
