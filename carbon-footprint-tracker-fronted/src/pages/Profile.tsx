18f5270161da7e809859136ed1a3187bac000ae5
Rupeshkumar663 <rupeshsamrat08@gmail.com>
2026-05-31 16:41:44 +0530

    Improve profile mobile responsiveness

--- carbon-footprint-tracker-fronted/src/pages/Profile.tsx
+++ carbon-footprint-tracker-fronted/src/pages/Profile.tsx
@@ -17,16 +17,16 @@ function Profile(){
 
   return (
     <div className="min-h-screen bg-gray-100 px-3 sm:px-4 py-6 sm:py-10 flex items-center justify-center">
-      <div className="bg-white shadow-lg rounded-2xl p-5 sm:p-8 max-w-xl w-full relative">
+      <div className="bg-white shadow-lg rounded-2xl p-4 sm:p-6 max-w-md w-full relative">
         <FaArrowLeftLong className="absolute top-5 left-5 w-[22px] h-[22px] cursor-pointer text-green-700" onClick={()=>navigate("/")}/>
         <div className="flex flex-col items-center text-center">
           {userData.photoUrl ? (
-            <img src={userData.photoUrl} className="w-24 h-24 rounded-full object-cover border-4 border-green-800" alt="Profile"  onError={(e)=> {e.currentTarget.style.display="none";}}/>
+            <img src={userData.photoUrl} className="w-20 h-20 sm:w-24 sm:h-24 rounded-full object-cover border-4 border-green-800" alt="Profile"  onError={(e)=> {e.currentTarget.style.display="none";}}/>
           ) : (
-            <div className="w-24 h-24 rounded-full text-white flex items-center justify-center text-[30px] border-2 bg-green-500 border-white"> {userData.name?.slice(0, 1).toUpperCase()}</div>
+            <div className="w-20 h-20 sm:w-24 sm:h-24 rounded-full text-white flex items-center justify-center text-[30px] border-2 bg-green-500 border-white"> {userData.name?.slice(0, 1).toUpperCase()}</div>
           )}
 
-          <h2 className="text-2xl font-bold mt-4 text-green-600">{userData.name}</h2>
+          <h2 className="text-xl sm:text-2xl font-bold mt-4 text-green-600">{userData.name}</h2>
           <p className="text-sm text-green-500">Carbon Saver 🌿</p>
         </div>
         <div className="mt-6 space-y-4">
@@ -41,7 +41,7 @@ function Profile(){
         </div>
         <div className="mt-6 flex justify-center gap-4">
           <button
-           className="px-4 py-2 border border-green-400 text-green-400 rounded-lg font-medium hover:bg-green-400 hover:text-black transition" onClick={() => navigate("/editprofile")} > Edit Profile
+           className="w-full sm:w-auto px-4 py-2 border border-green-400 text-green-400 rounded-lg font-medium hover:bg-green-400 hover:text-black transition" onClick={() => navigate("/editprofile")} > Edit Profile
           </button>
         </div>
       </div>
